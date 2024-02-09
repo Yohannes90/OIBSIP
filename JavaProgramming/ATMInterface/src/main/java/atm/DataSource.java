083836cf6316dc143bc8cabf17887c87bf64c8a4
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-02-09 19:03:19 +0300

    updaing datasource by adding get account from data base and testing it with random customer's account

--- JavaProgramming/ATMInterface/src/main/java/atm/DataSource.java
+++ JavaProgramming/ATMInterface/src/main/java/atm/DataSource.java
@@ -1,6 +1,5 @@
 package src.main.java.atm;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -12,10 +11,10 @@ public class DataSource {
     public static Connection connect() {
         String dbFile = "jdbc:sqlite:resources/bank.db";
         Connection connection = null;
-        try{
+        try {
             connection = DriverManager.getConnection(dbFile);
             System.out.println("Connected to Database");
-        } catch(SQLException e) {
+        } catch (SQLException e) {
             System.out.println("Connection to Database Failed");
             e.printStackTrace();
         }
@@ -26,31 +25,56 @@ public class DataSource {
         String sql = "SELECT * FROM CUSTOMERS WHERE userName = ?";
         Customer customer = null;
         try (Connection connection = connect();
-            PreparedStatement statement = connection.prepareStatement(sql)) {
-                statement.setString(1, email);
-                try (ResultSet resultSet = statement.executeQuery()) {
-                    customer = new Customer(resultSet.getInt("id"),
-                            resultSet.getString("name"),
-                            resultSet.getString("userName"),
-                            resultSet.getString("password"),
-                            resultSet.getInt("account_id"));
-                }
-        } catch(SQLException e) {
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, email);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                customer = new Customer(resultSet.getInt("id"),
+                        resultSet.getString("name"),
+                        resultSet.getString("userName"),
+                        resultSet.getString("password"),
+                        resultSet.getInt("account_id"));
+            }
+        } catch (SQLException e) {
             System.out.println("Error on getCustomer");
             e.printStackTrace();
         }
         return customer;
     }
 
+    public static Account getAccount(int accountId) {
+        String sql = "SELECT * FROM accounts WHERE id = ?";
+        Account account = null;
+        try (Connection connection = connect();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, accountId);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                account = new Account(resultSet.getInt("id"),
+                        resultSet.getString("type"),
+                        resultSet.getDouble("balance"));
+            }
+        } catch (SQLException e) {
+            System.out.println("Error in getAccount");
+            e.printStackTrace();
+        }
+        return account;
+    }
 
     public static void main(String[] args) {
-        connect();
+
+        // Testing getCustomer
         Customer customer = getCustomer("dwhittingtoncz@mozilla.org");
         if (customer.getId() ==  1118) {
             System.out.println("Got Correct customer");
         } else {
             System.out.println("Error...Got Wrong customer");
+        }
 
+        // Testing getAccount
+        Account account = getAccount(43267);
+        if (customer.getAccountNo() == account.getId()) {
+            System.out.println("Got Correct Account");
+        } else {
+            System.out.println("Error...Got Wrong Account");
         }
 
     }
