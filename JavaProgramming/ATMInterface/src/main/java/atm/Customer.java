738f9b9898650a39c1bb269c31352cd6499d994d
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-02-09 14:49:43 +0300

    adding customer class

---
+++ JavaProgramming/ATMInterface/src/main/java/atm/Customer.java
@@ -0,0 +1,57 @@
+package src.main.java.atm;
+
+public class Customer {
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+    private int accountNo;
+
+    public Customer(int id, String name, String email, String password, int accountNo) {
+        setId(id);
+        setName(name);
+        setEmail(email);
+        setPassword(password);
+        setAccountNo(accountNo);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAccountNo() {
+        return accountNo;
+    }
+
+    public void setAccountNo(int accountNo) {
+        this.accountNo = accountNo;
+    }
+}
