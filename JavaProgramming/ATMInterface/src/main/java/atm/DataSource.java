5ccadc9f7f6fdb15d89f54c1a7ea410aa91e6221
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-02-10 01:26:02 +0300

    updating DataSource class to remove tests and move main method to Menu class

--- JavaProgramming/ATMInterface/src/main/java/atm/DataSource.java
+++ JavaProgramming/ATMInterface/src/main/java/atm/DataSource.java
@@ -11,6 +11,7 @@ public class DataSource {
     public static Connection connect() {
         String dbFile = "jdbc:sqlite:resources/bank.db";
         Connection connection = null;
+
         try {
             connection = DriverManager.getConnection(dbFile);
             System.out.println("Connected to Database");
@@ -24,6 +25,7 @@ public class DataSource {
     public static Customer getCustomer(String email) {
         String sql = "SELECT * FROM CUSTOMERS WHERE userName = ?";
         Customer customer = null;
+
         try (Connection connection = connect();
              PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setString(1, email);
@@ -35,7 +37,7 @@ public class DataSource {
                         resultSet.getInt("account_id"));
             }
         } catch (SQLException e) {
-            System.out.println("Error on getCustomer");
+            System.out.println("Invalid User name");
             e.printStackTrace();
         }
         return customer;
@@ -44,6 +46,7 @@ public class DataSource {
     public static Account getAccount(int accountId) {
         String sql = "SELECT * FROM accounts WHERE id = ?";
         Account account = null;
+
         try (Connection connection = connect();
              PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setInt(1, accountId);
@@ -53,13 +56,13 @@ public class DataSource {
                         resultSet.getDouble("balance"));
             }
         } catch (SQLException e) {
-            System.out.println("Error in getAccount");
+            System.out.println("Invalid Account");
             e.printStackTrace();
         }
         return account;
     }
 
-    public static void updateAccount(int accountId, double balance) {
+    public static void updateAccountBalance(int accountId, double balance) {
         String sql = "UPDATE accounts SET balance = ? WHERE id = ?";
         try (Connection connection = connect();
              PreparedStatement statement = connection.prepareStatement(sql)) {
@@ -67,40 +70,9 @@ public class DataSource {
             statement.setInt(2, accountId);
             statement.executeUpdate();
         } catch (SQLException e) {
-            System.out.println("Error in UpdateAccount");
+            System.out.println("Invalid Account");
             e.printStackTrace();
         }
     }
 
-    public static void main(String[] args) {
-
-        // Testing getCustomer
-        Customer customer = getCustomer("dwhittingtoncz@mozilla.org");
-        if (customer.getId() ==  1118) {
-            System.out.println("Got Correct customer");
-        } else {
-            System.out.println("Error...Got Wrong customer");
-        }
-
-        // Testing getAccount
-        Account account = getAccount(43267);
-        if (customer.getAccountNo() == account.getId()) {
-            System.out.println("Got Correct Account");
-        } else {
-            System.out.println("Error...Got Wrong Account");
-        }
-
-        // Testing updateAccount
-        Account account1 = getAccount(11963);
-        double balance = account1.getBalance();
-        updateAccount(11963, balance - 1500);
-        if (getAccount(11963).getBalance() == balance - 1500) {
-            System.out.println("Successfully Updated Account");
-            updateAccount(11963, balance);
-        } else {
-            System.out.println("Error...Updating Account");
-        }
-
-
-    }
 }
