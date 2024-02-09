80bd81e9903da3deb87d3aea1b5dd832986d5810
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-02-09 18:36:12 +0300

    updaing datasource by adding get customer from data base and testing it with random customer

--- JavaProgramming/ATMInterface/src/main/java/atm/DataSource.java
+++ JavaProgramming/ATMInterface/src/main/java/atm/DataSource.java
@@ -3,6 +3,8 @@ package src.main.java.atm;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 
@@ -20,7 +22,36 @@ public class DataSource {
         return connection;
     }
 
+    public static Customer getCustomer(String email) {
+        String sql = "SELECT * FROM CUSTOMERS WHERE userName = ?";
+        Customer customer = null;
+        try (Connection connection = connect();
+            PreparedStatement statement = connection.prepareStatement(sql)) {
+                statement.setString(1, email);
+                try (ResultSet resultSet = statement.executeQuery()) {
+                    customer = new Customer(resultSet.getInt("id"),
+                            resultSet.getString("name"),
+                            resultSet.getString("userName"),
+                            resultSet.getString("password"),
+                            resultSet.getInt("account_id"));
+                }
+        } catch(SQLException e) {
+            System.out.println("Error on getCustomer");
+            e.printStackTrace();
+        }
+        return customer;
+    }
+
+
     public static void main(String[] args) {
         connect();
+        Customer customer = getCustomer("dwhittingtoncz@mozilla.org");
+        if (customer.getId() ==  1118) {
+            System.out.println("Got Correct customer");
+        } else {
+            System.out.println("Error...Got Wrong customer");
+
+        }
+
     }
 }
